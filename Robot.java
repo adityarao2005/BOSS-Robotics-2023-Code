aa95cb2ad09ddd8d35d0d5ccf3b746e43f8d4f9d
adityarao2005 <82911340+adityarao2005@users.noreply.github.com>
2023-02-16 17:00:30 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,25 +1,61 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*
+  2022 everybot code
+  written by carson graf 
+  don't email me, @ me on discord
+*/
+
+/*
+  This is catastrophically poorly written code for the sake of being easy to follow
+  If you know what the word "refactor" means, you should refactor this code
+*/
 
 package frc.robot;
 
+//import com.ctre.phoenix.motorcontrol.ControlMode;
+
+//import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+//import com.ctre.phoenix.motorcontrol.can.VictorSP;
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMax.IdleMode;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-
-  private RobotContainer m_robotContainer;
-
   
+  //Definitions for the hardware. Change this if you change what stuff you have plugged in
+  // This is wheels
+ VictorSP driveLeftA = new VictorSP(0);
+ VictorSP driveLeftB = new VictorSP(1);
+ VictorSP driveRightA = new VictorSP(3);
+ VictorSP driveRightB = new VictorSP(2);
+ 
+  //CANSparkMax arm = new CANSparkMax(5, MotorType.kBrushless);
+  //VictorSPX intake = new VictorSPX(6);
+
+  // joysticks
+  Joystick driverController = new Joystick(0);
+  
+
+  //Constants for controlling the arm. consider tuning these for your particular robot
+  //final double armHoldUp = 0.08;
+  //final double armHoldDown = 0.13;
+  //final double armTravel = 0.5;
+
+  //final double armTimeUp = 0.5;
+  //final double armTimeDown = 0.35;
+
+  //Varibles needed for the code
+  //boolean armUp = true; //Arm initialized to up because that's how it would start a match
+  //boolean burstMode = false;
+  //double lastBurstTime = 0;
+
+  double autoStart = 0;
+  boolean goForAuto = false;
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -27,79 +63,150 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    //Configure motors to turn correct direction. You may have to invert some of your motors
+    driveLeftA.setInverted(true);
+    //driveLeftA.burnFlash();
+    driveLeftB.setInverted(true);
+   // driveLeftB.burnFlash();
+    driveRightA.setInverted(false);
+    //driveRightA.burnFlash();
+    driveRightB.setInverted(false);
+    //driveRightB.burnFlash();
+    
+    //arm.setInverted(false);
+    //arm.setIdleMode(IdleMode.kBrake);
+    //arm.burnFlash();
+
+    //add a thing on the dashboard to turn off auto if needed
+    SmartDashboard.putBoolean("Go For Auto", false);
+    goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    //get a time for auton start to do events based on time later
+    autoStart = Timer.getFPGATimestamp();
+    //check dashboard icon to ensure good to do auto
+    goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+  public void autonomousPeriodic() {
+    //arm control code. same as in teleop
+    /*if(armUp){
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
+        arm.set(armTravel);
+      }
+      else{
+        arm.set(armHoldUp);
+      }
+    }
+    else{
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
+        arm.set(-armTravel);
+      }
+      else{
+        arm.set(-armHoldUp);
+      } 
+    }*/
+    
+    //get time since start of auto
+    double autoTimeElapsed = Timer.getFPGATimestamp() - autoStart;
+    if(goForAuto){
+      //series of timed events making up the flow of auto
+      if(autoTimeElapsed < 3){
+        //spit out the ball for three seconds
+        //intake.set(ControlMode.PercentOutput, -1);
+      }else if(autoTimeElapsed < 6){
+        //stop spitting out the ball and drive backwards *slowly* for three seconds
+        //intake.set(ControlMode.PercentOutput, 0);
+        driveLeftA.set(-0.3);
+        driveLeftB.set(-0.3);
+        driveRightA.set(-0.3);
+        driveRightB.set(-0.3);
+      }else{
+        //do nothing for the rest of auto
+       // intake.set(ControlMode.PercentOutput, 0);
+        driveLeftA.set(0);
+        driveLeftB.set(0);
+        driveRightA.set(0);
+        driveRightB.set(0);
+      }
     }
   }
 
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //Set up arcade steer
+    double forward = -driverController.getRawAxis(1);
+    double turn = -driverController.getRawAxis(5);//gamepad right joystick
+    
+    double driveLeftPower = forward - turn;
+    double driveRightPower = forward + turn;
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll(); 
+    double power = 0.1;
+
+    driveLeftA.set(driveLeftPower * power);
+    driveLeftB.set(driveLeftPower * power);
+    driveRightA.set(driveRightPower * power);
+    driveRightB.set(driveRightPower * power);
+
+    //Intake controls
+    /*if(driverController.getRawButton(5)){
+      intake.set(VictorSPXControlMode.PercentOutput, 1);;
+    }
+    else if(driverController.getRawButton(7)){
+      intake.set(VictorSPXControlMode.PercentOutput, -1);
+    }
+    else{
+      intake.set(VictorSPXControlMode.PercentOutput, 0);
+    } */
+
+    //Arm Controls
+    /*if(armUp){
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
+        arm.set(armTravel);
+      }
+      else{
+        arm.set(armHoldUp);
+      }
+    }
+    else{
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
+        arm.set(-armTravel);
+      }
+      else{
+        arm.set(-armHoldDown);
+      }
+    } */
+  
+   /* if(driverController.getRawButtonPressed(6) && !armUp){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = true;
+    }
+    else if(driverController.getRawButtonPressed(8) && armUp){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = false;
+    } */ 
+
   }
 
-  /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+  public void disabledInit() {
+    //On disable turn off everything
+    //done to solve issue with motors "remembering" previous setpoints after reenable
+    driveLeftA.set(0);
+    driveLeftB.set(0);
+    driveRightA.set(0);
+    driveRightB.set(0);
+    //arm.set(0);
+    //intake.set(ControlMode.PercentOutput, 0);
+  }
+    
 }
